5e88b597e6a4dbd5cca6cda7a41747f5f6d2ccff
DaniyarNur <51150792+DaniyarNur@users.noreply.github.com>
2021-05-08 02:05:49 +0600

    Add files via upload

---
+++ postgreSQL/delete.py
@@ -0,0 +1,34 @@
+import psycopg2
+from config import config
+
+def delete_vendor(vendor_id):
+    """ delete part by part id """
+    conn = None
+    rows_deleted = 0
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute("DELETE FROM vendors WHERE vendor_id = %s", (vendor_id,))
+        # get the number of updated rows
+        rows_deleted = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return rows_deleted
+
+
+if __name__ == '__main__':
+    deleted_rows = delete_vendor(8)
+    print('The number of deleted rows: ', deleted_rows)
---
+++ postgreSQL/query.py
@@ -0,0 +1,79 @@
+import psycopg2
+from config import config
+
+def get_vendors():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def get_parts():
+    """ query parts from the parts table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT part_id, part_name FROM parts ORDER BY part_name")
+        rows = cur.fetchall()
+        print("The number of parts: ", cur.rowcount)
+        for row in rows:
+            print(row)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def iter_row(cursor, size=10):
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
+
+def get_part_vendors():
+    """ query part and vendor data from multiple tables"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("""
+            SELECT part_name, vendor_name
+            FROM parts
+            INNER JOIN vendor_parts ON vendor_parts.part_id = parts.part_id
+            INNER JOIN vendors ON vendors.vendor_id = vendor_parts.vendor_id
+            ORDER BY part_name;
+        """)
+        for row in iter_row(cur, 10):
+            print(row)
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+if __name__ == '__main__':
+    get_vendors()
